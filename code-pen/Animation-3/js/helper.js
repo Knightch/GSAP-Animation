a8f53dfd490288b5305e3bd730893a70aa9a2665
MATHEMATICS TERRAINS <amankumarpandey2397@gmail.com>
2022-04-23 12:49:00 +0530

    animation 3 using barabr and gsap

---
+++ code-pen/Animation-3/js/helper.js
@@ -0,0 +1,8 @@
+function delay(n) {
+    n = n || 2000
+    // Keep official documentation wording, done -> resolve
+    // and make it more concise
+    return new Promise(resolve => {
+        setTimeout(resolve, n)
+    })
+}
